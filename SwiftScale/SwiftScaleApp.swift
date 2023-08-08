5f77611c105f832dc4d99f0c2473071b688aaac3
Nazarzbs <famina1998@gmail.com>
2023-08-08 17:22:34 +0300

    Initial Commit

---
+++ SwiftScale/SwiftScaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftScaleApp.swift
+//  SwiftScale
+//
+//  Created by Nazar on 08.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftScaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
